ab8a4da570435ef502ae9b4f9370a01cff4d5a8f
1101744096@qq.com <1101744096@qq.com>
2020-12-17 13:44:41 +0800

    fix(pom) cloud2020 pom文件jar包问题修复

--- cloud_pay_8001/src/main/java/com/young/springcloud/controller/PaymentController.java
+++ cloud_pay_8001/src/main/java/com/young/springcloud/controller/PaymentController.java
@@ -5,10 +5,7 @@ import com.young.springcloud.entities.Payment;
 import com.young.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @description:
@@ -24,7 +21,7 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping("/create")
-    public CommonResult create(Payment payment) {
+    public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         if (result > 0) {
             return new CommonResult(200, "插入数据成功", result);
