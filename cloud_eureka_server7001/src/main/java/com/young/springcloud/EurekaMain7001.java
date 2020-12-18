e23b2e925156e4389b7c94ac38b370cac2f0459f
1101744096@qq.com <1101744096@qq.com>
2020-12-18 10:00:43 +0800

    feat(eureka) 单机eureka服务搭建

--- cloud_eureka_server7001/src/main/java/com/young/springcloud/EurekaMain7001.java
+++ cloud_eureka_server7001/src/main/java/com/young/springcloud/EurekaMain7001.java
@@ -2,18 +2,19 @@ package com.young.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * @description: 启动类
+ * @description: Eureka服务启动类
  * @author: Young
- * @create: 2020-12-16 12:26
+ * @create: 2020-12-18 09:46
  * @Version: 1.0
  **/
+
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
 }
