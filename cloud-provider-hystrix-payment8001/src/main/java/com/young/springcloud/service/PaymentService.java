969d6ed0a46d0c1f225e601daaeed58ae80e1785
1101744096@qq.com <1101744096@qq.com>
2020-12-21 12:27:54 +0800

    feat(hystrix): 服务熔断

--- cloud-provider-hystrix-payment8001/src/main/java/com/young/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/young/springcloud/service/PaymentService.java
@@ -1,9 +1,11 @@
 package com.young.springcloud.service;
 
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.concurrent.TimeUnit;
 
@@ -46,6 +48,25 @@ public class PaymentService {
             e.printStackTrace();
         }
         return "线程池:  " + Thread.currentThread().getName() + "paymentInfo_ok,id:  " + id + "\t" + "哈哈~";
+    }
+
+    //服务熔断
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback",commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),//开启熔断器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),//请求总数阈值-请求次数超过峰值，熔断将会关闭
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),//快照时间窗-时间范围
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60"),//错误百分比阈值-失败率多少会跳闸
+    })
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
+        if (id < 0) {
+            throw new RuntimeException("*****id 不能为负数");
+        }
+        String serialNumber = IdUtil.simpleUUID();
+        return Thread.currentThread().getName() + "\t" + serialNumber;
+    }
+
+    public String paymentCircuitBreaker_fallback(@PathVariable("id") Integer id) {
+        return "id 不能为负数，请稍后再试 id:  "  + id;
     }
 
     public String paymentInfo_TimeOutHandler(Integer id) {
