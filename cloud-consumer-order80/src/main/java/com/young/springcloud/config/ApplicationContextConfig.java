c7becc285a99730f80b6390121dabe520c869ea5
1101744096@qq.com <1101744096@qq.com>
2020-12-22 15:43:53 +0800

    fix(sleuth链路跟踪): 完成sleuth配合zipkin实现链路追踪

--- cloud-consumer-order80/src/main/java/com/young/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/young/springcloud/config/ApplicationContextConfig.java
@@ -16,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 public class ApplicationContextConfig {
 
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
