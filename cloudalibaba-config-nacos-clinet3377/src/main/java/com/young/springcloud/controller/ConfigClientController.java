c75c51cc725437b8c588974314a4b74ab5ab35f9
1101744096@qq.com <1101744096@qq.com>
2020-12-24 16:27:27 +0800

    fix(nacos配置中心): nacos配置中心搭建
    nacos配置不同namespace，group，dataid，读取配置文件

---
+++ cloudalibaba-config-nacos-clinet3377/src/main/java/com/young/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,26 @@
+package com.young.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: Young
+ * @create: 2020-12-24 15:18
+ * @Version: 1.0
+ **/
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
