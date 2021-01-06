c66dae023082189a5c668bea1b305f6f847379d0
1101744096@qq.com <1101744096@qq.com>
2021-01-06 19:41:55 +0800

    fix(seata): seata依赖修改

--- seata-order-service2001/src/main/java/com/young/springcloud/SeataOrderMain2001.java
+++ seata-order-service2001/src/main/java/com/young/springcloud/SeataOrderMain2001.java
@@ -2,7 +2,6 @@ package com.young.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -12,7 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @create: 2021-01-06 12:17
  * @Version: 1.0
  **/
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 public class SeataOrderMain2001 {
